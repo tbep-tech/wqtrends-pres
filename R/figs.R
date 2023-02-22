70047256aa2d323bd43f6209f70ec2484220f10d
fawda123 <mbafs2012@gmail.com>
2023-02-22 12:21:37 -0500

    working on trends map

--- R/figs.R
+++ R/figs.R
@@ -247,3 +247,116 @@ dev.off()
 png('figs/graphabp5.png', height = hi, width = wd, family = 'serif', units = 'in', res = 300)
 p5
 dev.off()
+
+# trends map ----------------------------------------------------------------------------------
+
+library(tidyverse)
+library(wqtrends)
+library(here)
+library(sf)
+library(tbeptools)
+library(ggmap)
+
+load(file = here('data/tbchlamod.RData'))
+load(file = here('data/tbtnmod.RData'))
+
+tbchlamod <- tbchlamod %>% 
+  mutate(
+    param = 'chla'
+  )
+tbtnmod <- tbtnmod %>% 
+  mutate(
+    param = 'tn'
+  )
+
+modfit <- bind_rows(tbchlamod, tbtnmod) %>% 
+  mutate(
+    trn = purrr::pmap(list(station, param, mod), function(station, param, mod){
+      
+      cat(station, param, 'all \n')
+      
+      allcoefs <- anlz_metseason(mod, metfun = max, doystr = 32, doyend = 91, nsim = 100) %>% 
+        anlz_mixmeta(., yrstr = 2012, yrend = 2022) %>% 
+        summary() %>% 
+        .$coefficients 
+      
+      pval <- allcoefs[2, 4]
+      slos <- allcoefs[2, 1]
+      
+      outall <- tibble(
+        pval = pval, 
+        slos = slos,
+        seas = 'all'
+      )
+      
+      sumcoefs <- anlz_metseason(mod, metfun = max, doystr = 182, doyend = 304, nsim = 100) %>% 
+        anlz_mixmeta(., yrstr = 2012, yrend = 2022) %>% 
+        summary() %>% 
+        .$coefficients 
+      
+      pval <- sumcoefs[2, 4]
+      slos <- sumcoefs[2, 1]
+      
+      outsum <- tibble(
+        pval = pval, 
+        slos = slos, 
+        seas = 'summer'
+      )
+      
+      out <- bind_rows(outall, outsum)
+      
+      return(out)
+      
+    })
+  ) %>% 
+  select(-data, -mod) %>% 
+  unnest(trn)
+
+# basemap
+dat_ext <- unname(st_bbox(tbseg))
+bsmap1 <- get_stamenmap(bbox = dat_ext, maptype = 'terrain-background', zoom = 11)
+
+# change opacity of basemap
+mapatt <- attributes(bsmap1)
+bsmap1_transparent <- matrix(adjustcolor(bsmap1, 
+                                         alpha.f = 0.4), 
+                             nrow = nrow(bsmap1))
+attributes(bsmap1_transparent) <- mapatt
+
+chlleglab <- expression(paste(log[10], " chl-a change (", italic(mu), "g ", L^-1, yr^-1, ")"))
+tnleglab <- expression(paste("TN change (mg ", L^-1, yr^-1, ")"))
+
+pthm <- theme_bw(base_family = 'serif', base_size = 14) +
+  theme(
+    legend.position = 'top',
+    legend.box = 'vertical', 
+    strip.background = element_blank(),
+    axis.title = element_blank(), 
+    axis.text = element_text(size = 8)
+  )
+
+toplo <- modfit %>%
+  mutate(
+    pvalcol = ifelse(pval < 0.05, T, F), 
+    coefsgn = sign(slos), 
+    coefsgn = factor(coefsgn, levels = c('1', '-1'), labels = c('inc', 'dec')), 
+    seas = factor(seas, levels = c('all', 'summer'), labels = c('whole year', 'Summer'))
+  ) %>% 
+  left_join(stations, by = c('segment' = 'bay_segment', 'station' = 'epchc_station'))
+
+toplo1 <- toplo %>% 
+  filter(param == 'chla')
+
+p <- ggmap(bsmap1_transparent) +
+  geom_point(data = toplo1, aes(x = Longitude, y = Latitude, size = abs(slos), fill = slos, shape = coefsgn, color = pvalcol), stroke = 1) +
+  facet_grid(~ seas) + 
+  scale_fill_gradient2(chlleglab, low = 'green', mid = 'grey',  high = 'tomato1', midpoint = 0) +
+  scale_color_manual(values = c(scales::alpha('black', 0), 'black'), guide = 'none') +
+  coord_map() + 
+  scale_shape_manual(values = c(24, 25), guide = 'none') +
+  pthm +
+  scale_size(range = c(1, 6), guide = F) +
+  guides(fill = guide_colourbar(barheight = 0.5, barwidth = 8)) + 
+  labs(
+    caption = 'Outlines indicate p < 0.05'
+  )
