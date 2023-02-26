ea7599965b1f3cf1ed45acfc5ce5fd2db6d38336
fawda123 <mbafs2012@gmail.com>
2023-02-26 10:19:18 -0500

    kendall v gam trnd comparison figure done

--- R/figs.R
+++ R/figs.R
@@ -6,6 +6,8 @@ library(sf)
 library(tbeptools)
 library(ggmap)
 library(patchwork)
+library(EnvStats)
+library(lubridate)
 
 # gam example ---------------------------------------------------------------------------------
 
@@ -273,7 +275,7 @@ modfit <- bind_rows(tbchlamod, tbtnmod) %>%
       
       cat(station, param, 'all \n')
       
-      coefs1 <- anlz_metseason(mod, metfun = mean, doystr = 1, doyend = 364, nsim = 100) %>% 
+      coefs1 <- anlz_metseason(mod, metfun = mean, doystr = 1, doyend = 364, nsim = 1000) %>% 
         anlz_mixmeta(., yrstr = 2012, yrend = 2022) %>% 
         summary() %>% 
         .$coefficients 
@@ -287,7 +289,7 @@ modfit <- bind_rows(tbchlamod, tbtnmod) %>%
         seas = 'seas1'
       )
       
-      coefs2 <- anlz_metseason(mod, metfun = mean, doystr = 182, doyend = 304, nsim = 100) %>% 
+      coefs2 <- anlz_metseason(mod, metfun = mean, doystr = 182, doyend = 304, nsim = 1000) %>% 
         anlz_mixmeta(., yrstr = 2012, yrend = 2022) %>% 
         summary() %>% 
         .$coefficients 
@@ -353,9 +355,9 @@ p1 <- ggmap(bsmap1_transparent) +
   geom_point(data = toplo1, aes(x = Longitude, y = Latitude, size = abs(slos), fill = slos, shape = coefsgn, color = pvalcol), stroke = 1) +
   facet_grid(~ seas) + 
   scale_fill_gradient2(tnleglab, low = 'green', mid = 'grey',  high = 'tomato1', midpoint = 0) +
-  scale_color_manual(values = c(scales::alpha('black', 0), 'black'), guide = 'none') +
+  scale_color_manual(values = c(scales::alpha('black', 0), 'black'), guide = 'none', drop = FALSE) +
   coord_map() + 
-  scale_shape_manual(values = c(24, 25), guide = 'none') +
+  scale_shape_manual(values = c(24, 25), guide = 'none', drop = FALSE) +
   pthm +
   scale_size(range = c(1, 6), guide = F) +
   guides(fill = guide_colourbar(barheight = 0.7, barwidth = 16, title.position = 'top', title.hjust = 0.5)) 
@@ -364,9 +366,9 @@ p2 <- ggmap(bsmap1_transparent) +
   geom_point(data = toplo2, aes(x = Longitude, y = Latitude, size = abs(slos), fill = slos, shape = coefsgn, color = pvalcol), stroke = 1) +
   facet_grid(~ seas) + 
   scale_fill_gradient2(chlleglab, low = 'green', mid = 'grey',  high = 'tomato1', midpoint = 0) +
-  scale_color_manual(values = c(scales::alpha('black', 0), 'black'), guide = 'none') +
+  scale_color_manual(values = c(scales::alpha('black', 0), 'black'), guide = 'none', drop = FALSE) +
   coord_map() + 
-  scale_shape_manual(values = c(24, 25), guide = 'none') +
+  scale_shape_manual(values = c(24, 25), guide = 'none', drop = FALSE) +
   pthm +
   scale_size(range = c(1, 6), guide = F) +
   guides(fill = guide_colourbar(barheight = 0.7, barwidth = 16, title.position = 'top', title.hjust = 0.5)) + 
@@ -376,6 +378,131 @@ p2 <- ggmap(bsmap1_transparent) +
 
 p <- p1 + p2 + plot_layout(ncol = 2)
 png('figs/trndgam.png', height = 6, width = 11, family = 'serif', units = 'in', res = 300)
+p
+dev.off()
+
+# kendall and mixmeta trend -----------------------------------------------
+
+load(file = here('data/tbchlamod.RData'))
+load(file = here('data/tbtnmod.RData'))
+
+tbchlamod <- tbchlamod %>% 
+  mutate(
+    param = 'chla'
+  )
+tbtnmod <- tbtnmod %>% 
+  mutate(
+    param = 'tn'
+  )
+
+modcmp <- bind_rows(tbchlamod, tbtnmod) %>% 
+  mutate(
+    trn = purrr::pmap(list(station, param, mod, data), function(station, param, mod, data){
+      
+      cat(station, param, 'all \n')
+      
+      tomod <- data %>% 
+        filter(yr > 2011) %>% 
+        mutate(
+          mo = month(date)
+        )
+      
+      # run sk test
+      ests_sk <- kendallSeasonalTrendTest(log10(value) ~ mo + yr, data = tomod)
+      
+      outsk <- tibble(
+        pval = ests_sk$p.value[2], 
+        slos = ests_sk$estimate[2],
+        test = 'sk'
+      )
+      
+      # run meta test
+      coefs1 <- anlz_metseason(mod, metfun = mean, doystr = 1, doyend = 364, nsim = 1000) %>% 
+        anlz_mixmeta(., yrstr = 2012, yrend = 2022) %>% 
+        summary() %>% 
+        .$coefficients 
+      
+      outmt <- tibble(
+        pval = coefs1[2, 4],
+        slos = coefs1[2, 1], 
+        test = 'mt'
+      )
+      
+      out <- bind_rows(outsk, outmt)
+      
+      return(out)
+      
+    })
+  ) %>% 
+  select(-data, -mod) %>% 
+  unnest(trn)
+
+# basemap
+dat_ext <- unname(st_bbox(tbseg))
+bsmap1 <- get_stamenmap(bbox = dat_ext, maptype = 'terrain-background', zoom = 11)
+
+# change opacity of basemap
+mapatt <- attributes(bsmap1)
+bsmap1_transparent <- matrix(adjustcolor(bsmap1, 
+                                         alpha.f = 0.4), 
+                             nrow = nrow(bsmap1))
+attributes(bsmap1_transparent) <- mapatt
+
+chlleglab <- expression(paste(log[10], " chl-a change (", italic(mu), "g ", L^-1, yr^-1, ")"))
+tnleglab <- expression(paste(log[10], " TN change (mg ", L^-1, yr^-1, ")"))
+
+pthm <- theme_bw(base_family = 'serif', base_size = 14) +
+  theme(
+    legend.position = 'top',
+    legend.box = 'vertical', 
+    strip.background = element_blank(),
+    axis.title = element_blank(), 
+    axis.text = element_text(size = 9),
+    strip.text = element_text(size = 14)
+  )
+
+toplo <- modcmp %>%
+  mutate(
+    pvalcol = ifelse(pval < 0.05, T, F), 
+    coefsgn = sign(slos), 
+    coefsgn = factor(coefsgn, levels = c('1', '-1'), labels = c('inc', 'dec')), 
+    test = factor(test, levels = c('sk', 'mt'), labels = c('Seasonal Kendall', 'GAM/Mixed'))
+  ) %>% 
+  left_join(stations, by = c('segment' = 'bay_segment', 'station' = 'epchc_station'))
+
+toplo1 <- toplo %>% 
+  filter(param == 'tn')
+
+toplo2 <- toplo %>% 
+  filter(param == 'chla')
+
+p1 <- ggmap(bsmap1_transparent) +
+  geom_point(data = toplo1, aes(x = Longitude, y = Latitude, size = abs(slos), fill = slos, shape = coefsgn, color = pvalcol), stroke = 1) +
+  facet_grid(~ test) + 
+  scale_fill_gradient2(tnleglab, low = 'green', mid = 'grey',  high = 'tomato1', midpoint = 0) +
+  scale_color_manual(values = c(scales::alpha('black', 0), 'black'), guide = 'none', drop = FALSE) +
+  coord_map() + 
+  scale_shape_manual(values = c(24, 25), guide = 'none', drop = FALSE) +
+  pthm +
+  scale_size(range = c(1, 6), guide = F) +
+  guides(fill = guide_colourbar(barheight = 0.7, barwidth = 16, title.position = 'top', title.hjust = 0.5)) 
+
+p2 <- ggmap(bsmap1_transparent) +
+  geom_point(data = toplo2, aes(x = Longitude, y = Latitude, size = abs(slos), fill = slos, shape = coefsgn, color = pvalcol), stroke = 1) +
+  facet_grid(~ test) + 
+  scale_fill_gradient2(chlleglab, low = 'green', mid = 'grey',  high = 'tomato1', midpoint = 0) +
+  scale_color_manual(values = c(scales::alpha('black', 0), 'black'), guide = 'none', drop = FALSE) +
+  coord_map() + 
+  scale_shape_manual(values = c(24, 25), guide = 'none', drop = FALSE) +
+  pthm +
+  scale_size(range = c(1, 6), guide = F) +
+  guides(fill = guide_colourbar(barheight = 0.7, barwidth = 16, title.position = 'top', title.hjust = 0.5)) + 
+  labs(
+    caption = 'Outlines indicate p < 0.05'
+  )
+
+p <- p1 + p2 + plot_layout(ncol = 2)
+png('figs/trndcmp.png', height = 6, width = 11, family = 'serif', units = 'in', res = 300)
 p
 dev.off()
 
